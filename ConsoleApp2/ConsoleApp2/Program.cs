a9c9b184f69ac4da8eed0d4d0fd22808ce31b399
janpi <janpi@DESKTOP-DNO5S49>
2023-06-23 08:43:01 +0200

    Zadanie dzień 4

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,30 @@
+﻿string name = "Ewa";
+bool ismale = false;
+int age = 33; 
+
+if (!ismale)
+{
+    if (age == 33 && name == "Ewa")
+    {
+        Console.WriteLine("Ewa, lat 33");
+    }
+    else if (age < 30)
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+    else
+    {
+        Console.WriteLine("Kobieta powyżej 30 lat");
+    }
+}
+else
+{
+    if (age >= 18)
+    {
+        Console.WriteLine("Pełnolietni mężczyzna");
+    }
+    else
+    {
+        Console.WriteLine("Niepełnoletni mężczyzna");
+    }
+}
