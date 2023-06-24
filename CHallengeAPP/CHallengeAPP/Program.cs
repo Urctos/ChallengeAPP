df3fbf93bb23b21a2997c027a0a3460d373d5351
janpi <janpi@DESKTOP-DNO5S49>
2023-06-24 22:32:24 +0200

    Zadanie domowe 5

--- CHallengeAPP/CHallengeAPP/Program.cs
+++ CHallengeAPP/CHallengeAPP/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+int number = 564888;
+string numberInString = number.ToString();
+char[] letters = numberInString.ToArray();
+int[] outcome = new int[10];
+char[] meter = { '0', '1', '2', '3', '4', '5', '6' , '7', '8', '9' };
+
+foreach (char number2 in numberInString)
+{
+    for(int i = 0; i < meter.Length; i++)
+    {
+        if (meter[i] == number2)
+        {
+            outcome[i]++;
+            
+        }
+
+    }
+}
+for (int i = 0; i < outcome.Length; i ++)
+{
+    Console.WriteLine(meter[i] + " => " + outcome[i]);
+}
