6e2dd2b738c18c7d6d29fa84d95a74e785a821c9
janpi <janpi@DESKTOP-DNO5S49>
2023-07-13 22:18:00 +0200

    Praca domowa 10

--- CHallengeAPP/CHallengeAPP/Employee.cs
+++ CHallengeAPP/CHallengeAPP/Employee.cs
@@ -6,26 +6,30 @@ using System.Threading.Tasks;
 
 namespace CHallengeAPP
 {
-    internal class Employee
+    public class Employee
     {
-        private  List<int>grade = new List<int>(); 
+          
+        private  List<float>grade = new List<float>(); 
         public Employee(string name, string surname, int age)
         {
             this.Name = name;
             this.Surname = surname;
             this.Age = age;
+        }
+
+        public Employee(string name, string surname)
+        {
+            this.Name = name;
+            this.Surname = surname;
             
-
-
-
         }
 
         public string Name { get; private set; }
         public string Surname { get; private set; }
         public int Age { get; private set; }
-        public int Grade { get; private set; }
+        //public int Grade { get; private set; }
 
-        public int Result
+        public float Result
         {
             get
             {
@@ -33,9 +37,63 @@ namespace CHallengeAPP
             }
         }
 
-        public void AddGrade(int number)
+        public Statistics GetStatistics()
         {
-            this.grade.Add(number);
+            var statistics = new Statistics();
+            statistics.Average = 0;
+            statistics.Max = float.MinValue;
+            statistics.Min = float.MaxValue;
+
+            foreach(var grade in this.grade)
+            {
+                statistics.Max = Math.Max(statistics.Max, grade);
+                statistics.Min = Math.Min(statistics.Min, grade);
+                statistics.Average += grade;
+            }
+
+            statistics.Average = statistics.Average / this.grade.Count;         
+            return statistics;
         }
+
+        public void AddGrade(float grade)
+        {
+            if(grade >= 0 && grade <= 100)
+            {
+                this.grade.Add(grade);
+            }
+            else
+            {
+                Console.WriteLine("Invalid grade value");
+            }
+        }
+
+        public void AddGrade(string grade)
+        {
+            if(float.TryParse(grade, out float result))
+            {
+                //var value = float.Parse(grade);
+                //this.AddGrade(value);
+                this.AddGrade(result);
+            }
+            else
+            {
+                Console.WriteLine("String is not float");
+            }
+           
+        }
+
+        public void AddGrade(long grade)
+        {
+            float result = (float)grade;
+            this.AddGrade(result);
+        }
+
+        public void AddGrade(double grade)
+        {
+            float result = (float)Math.Round(grade, 2);
+            this.AddGrade(result);
+        }
+
+
     }
 }
--- CHallengeAPP/CHallengeAPP/Program.cs
+++ CHallengeAPP/CHallengeAPP/Program.cs
@@ -1,68 +1,20 @@
-﻿// Klasa np użykownik systemu czy zamówienie 
+﻿using CHallengeAPP;
 
-using CHallengeAPP;
+var employee = new Employee("Jan", "Kowalski");
 
-/*User user1 = new User("Ignacy", "434235");
-User user2 = new User("Jan", "434235");
-User user3 = new User("Ola", "434235");
-User user4 = new User("Aleksander", "434235");
+employee.AddGrade(2);
+employee.AddGrade(5);
+employee.AddGrade(6.356);
+employee.AddGrade(1555.34254);
 
-//user1.login = "Jan";
+var statistics = employee.GetStatistics();
 
-user2.AddScore(5);
-user2.AddScore(2);
-
-var result = user2.Result;
-Console.WriteLine(result);
-var name = User.GameName;
-var pi = Math.PI; */
-
-Employee nr1 = new Employee("Jan", "Kowalski", 23);
-Employee nr2 = new Employee("MIchał", "Wolodyjowski", 45);
-Employee nr3 = new Employee("Jeremi", "Wiśniowiecki", 47);
-
-nr1.AddGrade(3);
-nr1.AddGrade(7);
-nr1.AddGrade(4);
-nr1.AddGrade(2);
-nr1.AddGrade(4);
-
-nr2.AddGrade(3);
-nr2.AddGrade(3);
-nr2.AddGrade(3);
-nr2.AddGrade(3);
-nr2.AddGrade(3);
-
-nr3.AddGrade(1);
-nr3.AddGrade(3);
-nr3.AddGrade(2);
-nr3.AddGrade(5);
-nr3.AddGrade(3);
-
-//var result = nr1.Result;
-
-List<Employee> employees = new List<Employee>()
-{
-    nr1, nr2, nr3
-};
-
-int maxGrade = -1;
-Employee nrWithMaxResult = null;
+//Console.WriteLine($"Average: " + statistics.Max);
+Console.WriteLine($"Average: {statistics.Average:N2} "); 
+Console.WriteLine($"Max: {statistics.Max} ");
+Console.WriteLine($"MIn: {statistics.Min} ");
 
 
-
-foreach (var nr in employees)
-{
-    if (nr.Result > maxGrade)
-    {
-        nrWithMaxResult = nr;
-        maxGrade = nr.Result;
-    }
-
-    
-}
-
-Console.WriteLine(nrWithMaxResult.Name);
 
 
 
