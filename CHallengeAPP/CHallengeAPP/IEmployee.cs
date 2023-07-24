a26b725f4d860137b7cdf0d8ad3c038fc99d43de
janpi <janpi@DESKTOP-DNO5S49>
2023-07-24 20:16:20 +0200

    Zadanie domowe 15

---
+++ CHallengeAPP/CHallengeAPP/IEmployee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CHallengeAPP
+{
+    public interface IEmployee
+    {
+        string Name { get; }
+        string Surname { get; }
+
+        Statistics GetStatistics();
+        void AddGrade(float grade);
+        void AddGrade(string grade);
+        void AddGrade(long grade);
+        void AddGrade(char grade);
+    }
+}
---
+++ CHallengeAPP/CHallengeAPP/Supervisor.cs
@@ -0,0 +1,75 @@
+﻿
+namespace CHallengeAPP
+{
+    internal class Supervisor : IEmployee
+    {
+        string IEmployee.Name => "Zenon";
+        string IEmployee.Surname => "Maj";
+
+        private List<float> grade = new List<float>();
+
+        public void AddGrade(string grade)
+        {
+            switch (grade)
+            {
+                case "6":
+                    this.grade.Add(100);
+                    break;
+                case "5":
+                    this.grade.Add(80);
+                    break;
+                case "4":
+                    this.grade.Add(60);
+                    break;
+                case "3":
+                    this.grade.Add(40);
+                    break;
+                case "-3":
+                case "3-":
+                    this.grade.Add(35);
+                    break;
+                case "2+":
+                case "+2":
+                    this.grade.Add(25);
+                    break;
+                case "2":
+                    this.grade.Add(20);
+                    break;
+                case "1":
+                    this.grade.Add(0);
+                    break;
+                default:
+                    throw new Exception("Wrong number");
+            }
+        }
+
+        public void AddGrade(float grade)
+        {
+            if (grade >= 0 && grade <= 100)
+            {
+                this.grade.Add(grade);
+            }
+            else
+            {
+                throw new Exception("invalid grade value");
+            }
+        }
+
+        public void AddGrade(long grade)
+        {
+            float result = (float)grade;
+            this.AddGrade(result);
+        }
+
+        void IEmployee.AddGrade(char grade)
+        {
+            throw new NotImplementedException();
+        }
+
+        Statistics IEmployee.GetStatistics()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
