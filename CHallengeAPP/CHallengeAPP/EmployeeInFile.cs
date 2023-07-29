51acfda1534a8d11a53dd1b16db28cbf1631778f
janpi <janpi@DESKTOP-DNO5S49>
2023-07-29 08:15:30 +0200

    Zadanie domowe 18

--- CHallengeAPP/CHallengeAPP/EmployeeInFile.cs
+++ CHallengeAPP/CHallengeAPP/EmployeeInFile.cs
@@ -3,7 +3,7 @@
     internal class EmployeeInFile : EmployeeBase
     {
         private const string fileName = "grades.txt";
-        public EmployeeInFile(string name, string surname) 
+        public EmployeeInFile(string name, string surname)
             : base(name, surname)
         {
 
@@ -13,7 +13,7 @@
 
         public override void AddGrade(float grade)
         {
-            if (grade >= 0 && grade <= 100 )
+            if (grade >= 0 && grade <= 100)
             {
                 using (var writer = File.AppendText(fileName))
                 {
@@ -77,7 +77,7 @@
                     break;
                 default:
                     throw new Exception("Wrong letter");
-            }    
+            }
         }
 
         public override void AddGrade(int grade)
@@ -90,73 +90,24 @@
 
         public override Statistics GetStatistics()
         {
-            var gradesFromFile = this.ReadGradesFromFile();
-            var result = this.CountStatistics(gradesFromFile);
-            return result;
-        }
+            var statistics = new Statistics();
 
-        private List<float> ReadGradesFromFile()
-        {
-            var grades = new List<float>();
             if (File.Exists(fileName))
             {
-                using (var reader = File.OpenText(fileName))
+                using ( var reader = File.OpenText(fileName))
                 {
                     var line = reader.ReadLine();
                     while (line != null)
                     {
                         var number = float.Parse(line);
-                        grades.Add(number);
+                        statistics.AddGrade(number);
                         line = reader.ReadLine();
+
                     }
-
                 }
-
             }
-            return grades;
-        }
-
-        private Statistics CountStatistics(List<float> grades)
-        {
-            var statistics = new Statistics();
-            statistics.Average = 0;
-            statistics.Max = float.MinValue;
-            statistics.Min = float.MaxValue;
-
-
-            foreach (var grade in grades)
-            {
-                if (grade >= 0)
-                {
-                    statistics.Max = Math.Max(statistics.Max, grade);
-                    statistics.Min = Math.Min(statistics.Min, grade);
-                    statistics.Average += grade;
-                }
-
-            }
-
-            statistics.Average = statistics.Average / grades.Count;
-
-            switch (statistics.Average)
-            {
-                case var average when average >= 80:
-                    statistics.AverageLetter = 'A';
-                    break;
-                case var average when average >= 60:
-                    statistics.AverageLetter = 'B';
-                    break;
-                case var average when average >= 40:
-                    statistics.AverageLetter = 'C';
-                    break;
-                case var average when average >= 20:
-                    statistics.AverageLetter = 'D';
-                    break;
-                default:
-                    statistics.AverageLetter = 'E';
-                    break;
-            }
-
             return statistics;
         }
+
     }
 }
